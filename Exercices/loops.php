0b8aa3546719e6234cd18e2d9419dec4ded6f05d
DelphineLecorney <lecorney.delphine@gmail.com>
2023-06-05 16:20:06 +0200

    Add exercise loop while

--- Exercices/loops.php
+++ Exercices/loops.php
@@ -8,12 +8,26 @@
 </head>
 <body>
     <?php
-        $pronouns = array ('I', 'You', 'He/She','We', 'You', 'They');
-        foreach($pronouns as $pronoun) {
-            $verbCode = ($pronoun === 'He/She') ? 'codes' : 'code';
-            echo "$pronoun $verbCode <br>";
-        }
+    $pronouns = array ('I', 'You', 'He/She','We', 'You', 'They');
 
+    // Using foreach, build a loop that displays each element of the array
+    // Then, modify your loop so as to conjugate the verb "to code" in the present tense. 
+    // Use a <br> to go to the next line.
+
+    foreach($pronouns as $pronoun) {
+        $verbCode = ($pronoun === 'He/She') ? 'codes' : 'code';
+        echo "$pronoun $verbCode <br>";
+    }
+    echo "<br><br>";
+    
+    // Write a script that prints the numbers from 1 to 120 using  while 
+
+    $numbers = 1;
+    while($numbers <= 120)
+    {
+        echo $numbers;
+        $numbers ++;
+    }
 
     ?>
 
