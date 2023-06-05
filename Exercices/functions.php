f02934d234e965dc3553fab67d2e6aa438162a9d
Del-Lec <102812607+Del-Lec@users.noreply.github.com>
2023-06-05 22:20:54 +0200

    Add function str_shuffle()

--- Exercices/functions.php
+++ Exercices/functions.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYphraseE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -7,6 +7,19 @@
     <title>Functions</title>
 </head>
 <body>
-    
+    <?php
+        
+        $phrase = 
+        "According to a researcher (sic) at Cambridge University, it doesn't matter 
+        in what order the letters in a word are, the only important thing is that the first 
+        and last letter be at the right place. The rest can be a total mess and you can still 
+        read it without problem. This is because the human mind does not read every letter by 
+        itself but the word as a whole.";
+    // Make sure that the separator parameter is placed before the string parameter.
+        $phrase = explode(" ", $phrase);
+        foreach($phrase as $tab){
+            echo str_shuffle($tab." ");
+        }        
+    ?>
 </body>
 </html>
