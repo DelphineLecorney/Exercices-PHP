9e4658a13bfe27f596329c64ed78b325e0acc467
DelphineLecorney <lecorney.delphine@gmail.com>
2023-06-06 14:02:16 +0200

    Add function replaceLetters

--- Exercices/functions.php
+++ Exercices/functions.php
@@ -20,24 +20,24 @@
     foreach($phrase as $tab){
         echo str_shuffle($tab." ");
     }   
-    echo "<br>";
+    echo '<br><br>';
 
     // Use a function that capitalizes the first letter of the provided argument. 
     // Example: "émile"should return "Émile"
     $name = "émile";
     echo mb_convert_case($name, MB_CASE_TITLE, "UTF-8");
 
-    echo "<br>";
+    echo '<br><br>';
     
     // Use the native function allowing you to display the current year.    
     echo date("Y");
 
-    echo "<br>";
+    echo '<br><br>';
 
     // Now display the date, time, minutes and seconds, using the same function, by playing with the arguments.
     echo date('l d m Y h:i:s');
 
-    echo "<br>";
+    echo '<br><br>';
 
     // Crée a "Sum" function that takes 2 numbers and returns their sum.
 
@@ -48,7 +48,7 @@
     $res = sum(5, 5);
     echo "$res";
 
-    echo "<br>";
+    echo '<br><br>';
 
     // Improve that function so that it checks whether the argument is indeed a Number. 
     // If not, it should display : "Error: argument is the not a number."
@@ -61,11 +61,12 @@
     $res2 = sum2("j", 20);
     echo "$res2";
 
-    echo '<br>';
+    echo '<br><br>';
 
     // Create a function that takes as argument a string of characters 
     // and returns an acronym made of the initials of each word.
     // Example: "In code we trust!" should return: ICWT).
+
     function initialsWord($string){
         $initial = '';
         $words = explode(" ", $string);
@@ -80,6 +81,20 @@
     $resPhrase = initialsWord($phrase);
     echo $resPhrase;
     
+    echo '<br><br>';
+    
+    // Create a function that replaces the letters "a" and "e" with "æ". 
+    function replaceLetters($words){
+            $words = str_replace("ae", "æ", $words);
+            return $words;
+        }
+    $wordsExample = 'caecotrophie, chaenichthys,microsphaera, sphaerotheca';
+    echo $wordsExample;
+    echo '<br>';
+
+    $resWords = replaceLetters($wordsExample);
+    echo $resWords
+
     ?>
 </body>
 </html>
